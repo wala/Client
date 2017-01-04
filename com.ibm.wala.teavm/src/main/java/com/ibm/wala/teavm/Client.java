f57ac023883c3673228bdd779099ff7c884fdc81
Julian Dolby <juliandolby@gmail.com>
2017-01-03 21:02:34 -0500

    added wala.util regression tests in javascript

--- com.ibm.wala.teavm/src/main/java/com/ibm/wala/teavm/Client.java
+++ com.ibm.wala.teavm/src/main/java/com/ibm/wala/teavm/Client.java
@@ -1,6 +1,5 @@
 package com.ibm.wala.teavm;
 
-import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.function.Function;
@@ -9,12 +8,19 @@ import org.teavm.jso.JSBody;
 import org.teavm.jso.JSFunctor;
 import org.teavm.jso.JSObject;
 import org.teavm.jso.browser.Window;
-import org.teavm.jso.core.JSArray;
 import org.teavm.jso.core.JSFunction;
 import org.teavm.jso.dom.html.HTMLButtonElement;
 import org.teavm.jso.dom.html.HTMLDocument;
 
+import com.ibm.wala.core.tests.basic.ExtensionGraphTest;
+import com.ibm.wala.core.tests.basic.FloydWarshallTest;
 import com.ibm.wala.core.tests.basic.GraphDataflowTest;
+import com.ibm.wala.core.tests.basic.OrdinalSetTest;
+import com.ibm.wala.core.tests.basic.PathFinderTest;
+import com.ibm.wala.core.tests.basic.PrimitivesTest;
+import com.ibm.wala.core.tests.basic.WelshPowellTest;
+import com.ibm.wala.core.tests.collections.SemiSparseMutableIntSetTest;
+import com.ibm.wala.core.tests.collections.TwoLevelVectorTest;
 import com.ibm.wala.util.CancelException;
 import com.ibm.wala.util.graph.Graph;
 import com.ibm.wala.util.graph.NumberedGraph;
@@ -27,94 +33,160 @@ import com.ibm.wala.util.graph.traverse.WelshPowell.ColoredVertices;
 import de.iterable.teavm.jso.browser.Console;
 
 public class Client {
-	
+
 	@JSBody(params = { "message" }, script = "callback(message)")
 	public static native void handle(String message);
-	
-    private static HTMLDocument document = Window.current().getDocument();
-    
-    private static HTMLButtonElement solveNodeButton = document.getElementById("solveNodes").cast();
-    private static HTMLButtonElement solveEdgeButton = document.getElementById("solveNodesAndEdges").cast();
-    
+
+	private static HTMLDocument document = Window.current().getDocument();
+
+	private static HTMLButtonElement solveNodeButton = document.getElementById("solveNodes").cast();
+	private static HTMLButtonElement solveEdgeButton = document.getElementById("solveNodesAndEdges").cast();
+
 	public static void main(String[] args)  {
-	    solveNodeButton.addEventListener("click", evt -> { 
-	    	solve((Graph<String> G) -> {
+		solveNodeButton.addEventListener("click", evt -> { 
+			solve((Graph<String> G) -> {
 				try {
 					return GraphDataflowTest.solveNodeOnly(G);
 				} catch (CancelException e) {
 					return "fail";
 				}
 			} );
-	    });
-		
-	    solveEdgeButton.addEventListener("click", evt -> { 
-	    	solve((Graph<String> G) -> {
+		});
+
+		solveEdgeButton.addEventListener("click", evt -> { 
+			solve((Graph<String> G) -> {
 				try {
 					return GraphDataflowTest.solveNodeEdge(G);
 				} catch (CancelException e) {
 					return "fail";
 				}
 			} );
-	    });
-	    	
-	    setSolver((Constraints system) -> {
-	    	SlowSparseNumberedGraph<JSWrapper> G = SlowSparseNumberedGraph.make();	
-	    	for(int i = 0; i < system.getSize(); i++) {
-	    		Constraint c = system.constraint(i);
-	    		JSWrapper sub = new JSWrapper(c.getSub());
-	    		if (! G.containsNode(sub)) { G.addNode(sub); }
-	    		JSWrapper sup = new JSWrapper(c.getSuper());
-	    		if (! G.containsNode(sup)) { G.addNode(sup); }
-	    		G.addEdge(sup, sub);
-	    	}
-
-	    	new WelshPowell<JSWrapper>().color(G).toString();
-	    	
-	    	return G;
-	    });
-	    
-	    setParent((Graph<JSWrapper> G, Hashable a, Hashable b, Hashable c) -> {
-	    	return leastCommonAncestor(G, new JSWrapper(a), new JSWrapper(b), new JSWrapper(c)).getObj();
-	    });
-	    
-	    setColor((NumberedGraph<JSWrapper> G) -> { 
-	    	ColoredVertices<JSWrapper> colors = (new WelshPowell<JSWrapper>()).color(G);
-			for(JSWrapper e : G) {
-				Console.log(e + ":" + colors.getColors().get(e));
-			}
-	    	return colors;
-	    });
-	    
-	    setCallback((Graph<JSWrapper> G, JSFunction f) -> {
-	    	for(Iterator<JSWrapper> I = Topological.makeTopologicalIter(G); I.hasNext(); ) {
-	    		f.call(I.next().getObj());
-	    	}
 		});
+
+		setSolver((Constraints system) -> {
+			SlowSparseNumberedGraph<JSWrapper> G = SlowSparseNumberedGraph.make();	
+			for(int i = 0; i < system.getSize(); i++) {
+				Constraint c = system.constraint(i);
+				JSWrapper sub = new JSWrapper(c.getSub());
+				if (! G.containsNode(sub)) { G.addNode(sub); }
+				JSWrapper sup = new JSWrapper(c.getSuper());
+				if (! G.containsNode(sup)) { G.addNode(sup); }
+				G.addEdge(sup, sub);
+			}
+
+			new WelshPowell<JSWrapper>().color(G).toString();
+
+			return G;
+		});
+
+		setParent((Graph<JSWrapper> G, Hashable a, Hashable b, Hashable c) -> {
+			return leastCommonAncestor(G, new JSWrapper(a), new JSWrapper(b), new JSWrapper(c)).getObj();
+		});
+
+		setColor((NumberedGraph<JSWrapper> G) -> { 
+			ColoredVertices<JSWrapper> colors = (new WelshPowell<JSWrapper>()).color(G);
+			return new GetColor() {
+				@Override
+				public int get(Hashable o) {
+					return colors.getColors().get(new JSWrapper(o));
+				}
+			};
+		});
+
+		setTopsort((Graph<JSWrapper> G) -> {
+			return Topological.makeTopologicalIter(G);
+		});
+
+		setForeach((Iterable<JSWrapper> iter, JSFunction f) -> {
+			for(JSWrapper e : iter) {
+				f.call(e.getObj());
+			}
+		});
+
+		walaUtilTests();
+	}
+
+	private static void walaUtilTests() {
+		runTest(() -> { (new ExtensionGraphTest()).testAugment(); });
+		runTest(() -> { (new FloydWarshallTest()).TestPathLengths(); });
+		runTest(() -> { (new FloydWarshallTest()).TestShortestPath(); });
+		runTest(() -> { (new FloydWarshallTest()).TestShortestPaths(); });
+		runTest(() -> { (new GraphDataflowTest()).testSolverNodeOnly(); });
+		runTest(() -> { (new GraphDataflowTest()).testSolverNodeEdge(); });
+		runTest(() -> { (new OrdinalSetTest()).test1(); });
+		runTest(() -> { (new PathFinderTest()).testPaths1(); });
+		runTest(() -> { (new PathFinderTest()).testPaths2(); });
+		runTest(() -> { (new PathFinderTest()).testPaths3(); });
+		runTest(() -> { (new PathFinderTest()).testPaths4(); });
+		runTest(() -> { (new PrimitivesTest()).testBFSPathFinder(); });
+		runTest(() -> { (new PrimitivesTest()).testBimodalMap(); });
+		runTest(() -> { (new PrimitivesTest()).testBimodalMutableSparseIntSet(); });
+		runTest(() -> { (new PrimitivesTest()).testBinaryIntegerRelation(); });
+		runTest(() -> { (new PrimitivesTest()).testBitVector(); });
+		runTest(() -> { (new PrimitivesTest()).testBitVectorIntSet(); });
+		runTest(() -> { (new PrimitivesTest()).testBitVectors(); });
+		runTest(() -> { (new PrimitivesTest()).testBoundedBFS(); });
+		runTest(() -> { (new PrimitivesTest()).testDominatorsA(); });
+		runTest(() -> { (new PrimitivesTest()).testMutableSharedBitVectorIntSet(); });
+		runTest(() -> { (new PrimitivesTest()).testMutableSparseIntSet(); });
+		// runTest(() -> { (new PrimitivesTest()).testMutableSparseLongSet(); });
+		runTest(() -> { (new PrimitivesTest()).testOffsetBitVector0_10(); });
+		runTest(() -> { (new PrimitivesTest()).testOffsetBitVector100_10(); });
+		runTest(() -> { (new PrimitivesTest()).testOffsetBitVector10_10(); });
+		runTest(() -> { (new PrimitivesTest()).testOffsetBitVector50_10(); });
+		runTest(() -> { (new PrimitivesTest()).testOffsetBitVector50_50(); });
+		runTest(() -> { (new PrimitivesTest()).testOffsetBitVectors100_200_10(); });
+		runTest(() -> { (new PrimitivesTest()).testOffsetBitVectors100_25_10(); });
+		runTest(() -> { (new PrimitivesTest()).testOffsetBitVectors150_10(); });
+		runTest(() -> { (new PrimitivesTest()).testOffsetBitVectors35_25_20_10(); });
+		runTest(() -> { (new PrimitivesTest()).testSemiSparseMutableIntSet(); });
+		runTest(() -> { (new PrimitivesTest()).testSmallMap(); });
+		runTest(() -> { (new PrimitivesTest()).testSpecificBugsInOffsetBitVectors(); });
+		runTest(() -> { (new PrimitivesTest()).testSpecificBugsInSemiSparseMutableIntSets(); });
+		runTest(() -> { (new PrimitivesTest()).testUnionFind(); });
+		runTest(() -> { (new WelshPowellTest()).testOne(); });
+		runTest(() -> { (new WelshPowellTest()).testTwo(); });
+		runTest(() -> { (new SemiSparseMutableIntSetTest()).testCase1(); });
+		runTest(() -> { (new TwoLevelVectorTest()).testCase1(); });
+	}
+
+	private interface Test {
+		void test() throws CancelException;
+	}
+
+	private static void runTest(Test test) {
+		Console.log("running " + test.getClass().getName());
+		try {
+			test.test();
+		} catch (Throwable e) {
+			Console.log("teat failed with " + e.toString());
+			e.printStackTrace();
+		}
 	}
 
 	private static JSWrapper leastCommonAncestor(Graph<JSWrapper> G, JSWrapper root, JSWrapper a, JSWrapper b) {
 		BFSPathFinder<JSWrapper> apf = new BFSPathFinder<JSWrapper>(G, root, a);
 		List<JSWrapper> ap = apf.find();
-		
+
 		BFSPathFinder<JSWrapper> bpf = new BFSPathFinder<JSWrapper>(G, root, b);
 		List<JSWrapper> bp = bpf.find();
-		
+
 		for(JSWrapper e : ap) {
 			if (bp.contains(e)) {
 				return e;
 			}
 		}
-		
+
 		throw new NoSuchElementException();
 	}
-	
+
 	public static void solve(Function<Graph<String>,String> f) {
 		Graph<String> G = GraphDataflowTest.buildGraph();
 		String solution = f.apply(G);
 		Console.log(solution);
 		handle(solution);
 	}
-		
+
 	@JSFunctor
 	public interface Parent extends JSObject {
 		Object parent(Graph<JSWrapper> G, Hashable root, Hashable a, Hashable b);
@@ -123,9 +195,14 @@ public class Client {
 	@JSBody(params = { "s" }, script = "parent = s;")
 	public static native void setParent(Parent s);
 
+	@JSFunctor 
+	public interface GetColor extends JSObject {
+		int get(Hashable o);
+	}
+
 	@JSFunctor
 	public interface Color extends JSObject {
-		ColoredVertices<JSWrapper> color(NumberedGraph<JSWrapper> G);
+		GetColor color(NumberedGraph<JSWrapper> G);
 	}
 
 	@JSBody(params = { "s" }, script = "color = s;")
@@ -138,12 +215,21 @@ public class Client {
 
 	@JSBody(params = { "s" }, script = "solver = s;")
 	public static native void setSolver(Solver s);
-	
+
 	@JSFunctor 
-	public interface Callback extends JSObject {
-		void cb(Graph<JSWrapper> G, JSFunction f);
+	public interface Topsort extends JSObject {
+		Iterable<JSWrapper> cb(Graph<JSWrapper> G);
 	}
 
 	@JSBody(params = { "s" }, script = "callback = s;")
-	public static native void setCallback(Callback s);
+	public static native void setTopsort(Topsort s);
+
+	@JSFunctor
+	public interface Foreach extends JSObject {
+		public void foreach(Iterable<JSWrapper> seq, JSFunction fun);
+	}
+
+	@JSBody(params = { "f" }, script = "foreach = f;")
+	public static native void setForeach(Foreach f);
+
 }
