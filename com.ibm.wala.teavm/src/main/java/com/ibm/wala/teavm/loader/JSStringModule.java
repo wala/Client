b323c0a112a40bf3cdc870bdc5618155ca2f8cd3
Julian Dolby <juliandolby@gmail.com>
2017-01-20 12:47:26 -0500

    move module files

---
+++ com.ibm.wala.teavm/src/main/java/com/ibm/wala/teavm/loader/JSStringModule.java
@@ -0,0 +1,69 @@
+package com.ibm.wala.teavm.loader;
+
+import java.io.InputStream;
+
+import org.teavm.jso.core.JSString;
+
+import com.ibm.wala.classLoader.Module;
+import com.ibm.wala.classLoader.ModuleEntry;
+
+public class JSStringModule implements ModuleEntry {
+	private final JSString fileName;
+	private final JSString contents;
+
+	public JSStringModule(JSString fileName, JSString contents) {
+		this.fileName = fileName;
+		this.contents = contents;
+	}
+
+	@Override
+	public String getName() {
+		return fileName.stringValue();
+	}
+
+	@Override
+	public boolean isClassFile() {
+		return false;
+	}
+
+	@Override
+	public boolean isSourceFile() {
+		return true;
+	}
+
+	@Override
+	public InputStream getInputStream() {
+		return new InputStream() {
+			private int i = 0;
+			@Override
+			public int read() {
+				if (i >= contents.getLength()) {
+					return -1;
+				} else {
+					return contents.charCodeAt(i++);
+				}
+			}
+		};
+	}
+
+	@Override
+	public boolean isModuleFile() {
+		return false;
+	}
+
+	@Override
+	public Module asModule() {
+		return null;
+	}
+
+	@Override
+	public String getClassName() {
+		return null;
+	}
+
+	@Override
+	public Module getContainer() {
+		return null;
+	}
+}
+
