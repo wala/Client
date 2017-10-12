483e3d44812e9dfdb171e34df02bb8c63709358e
Julian Dolby <juliandolby@gmail.com>
2017-10-11 22:19:37 -0400

    fixes for reading JSStrings in WALA

--- com.ibm.wala.teavm/src/main/java/com/ibm/wala/teavm/loader/JSStringModule.java
+++ com.ibm.wala.teavm/src/main/java/com/ibm/wala/teavm/loader/JSStringModule.java
@@ -1,15 +1,20 @@
 package com.ibm.wala.teavm.loader;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.Reader;
+import java.io.StringReader;
+import java.net.URL;
 import java.util.Iterator;
 
 import org.teavm.jso.core.JSString;
 
 import com.ibm.wala.classLoader.Module;
 import com.ibm.wala.classLoader.ModuleEntry;
+import com.ibm.wala.classLoader.SourceModule;
 import com.ibm.wala.util.collections.NonNullSingletonIterator;
 
-public class JSStringModule implements Module, ModuleEntry {
+public class JSStringModule implements Module, ModuleEntry, SourceModule {
 	private final JSString fileName;
 	private final JSString contents;
 
@@ -71,6 +76,40 @@ public class JSStringModule implements Module, ModuleEntry {
 	@Override
 	public Iterator<? extends ModuleEntry> getEntries() {
 		return new NonNullSingletonIterator<JSStringModule>(this);
+	}
+
+	@Override
+	public Reader getInputReader() {
+		return new Reader() {
+			int i = 0;
+
+			@Override
+			public int read(char[] cbuf, int off, int len) throws IOException {
+				if (i >= contents.getLength()) {
+					return -1;
+				}
+
+				int j;
+				for(j = 0; j < len; j++) {
+					if (i == contents.getLength()) {
+						return j;
+					}
+					cbuf[j+off] = (char) contents.charCodeAt(i++);
+				}
+				return j;
+			}
+
+			@Override
+			public void close() throws IOException {
+				// do nothing
+			}
+		};
+	}
+
+	@Override
+	public URL getURL() {
+		assert false;
+		return null;
 	}
 }
 
